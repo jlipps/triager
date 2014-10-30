601fa8610669ba54435c83b0b8b61d45ae614ca7
Jonathan Lipps <jlipps@gmail.com>
2014-10-30 16:39:08 -0700

    add hash-bang to bin files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 "use strict";
 
 var gulp = require('gulp')
+  , header = require('gulp-header')
+  , filter = require('gulp-filter')
   , traceur = require('gulp-traceur');
 
 gulp.task('default', function () {
@@ -11,8 +13,14 @@ gulp.task('default', function () {
     annotations: true,
     arrayComprehension: true
   };
+  var binFilter = filter(function (file) {
+    return /(main|retro)\.js/.test(file.path);
+  });
   return gulp.src('lib/es6/**/*.js')
              .pipe(traceur(traceurOpts))
+             .pipe(binFilter)
+                .pipe(header('#!/usr/bin/env node\n'))
+                .pipe(binFilter.restore())
              .pipe(gulp.dest('lib/es5'));
 });
 
